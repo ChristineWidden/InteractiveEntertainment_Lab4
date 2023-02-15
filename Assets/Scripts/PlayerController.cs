a3ed71b3dcfa9c33083b1c92d2ad13b2e3559081
TheShinySnivy <magicwand452@gmail.com>
2023-02-15 11:01:35 -0800

    movement adjustments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,34 +15,52 @@ public class PlayerController : MonoBehaviour
     private bool onGround;
     public float jumpForce;
 
+    public float stopThreshold;
+
     private Rigidbody2D rb;
+    private Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Jump") && onGround) {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        }
+
+        setAnimations();
+    }
+
+    void FixedUpdate() {
         HInput = Input.GetAxis("Horizontal");
-        HInput = HInput == 0 ? 0 : Mathf.Sign(HInput);
+        //HInput = HInput == 0 ? 0 : Mathf.Sign(HInput);
 
-        float directionMultiplier = Mathf.Round(rb.velocity.x) == 0 ? 0 : Mathf.Sign(rb.velocity.x);
+        float directionMultiplier = Mathf.Abs(HInput) < stopThreshold ? 0 : Mathf.Sign(rb.velocity.x);
 
+        //float friction = Mathf.Abs(rb.velocity.x) < 0.1 ? groundFriction * 1.5f : groundFriction;
 
-        float newVelocity = Math.Clamp(rb.velocity.x + (HInput * acceleration) - (onGround ? 1 : 0) * directionMultiplier * groundFriction, -1 * maxSpeed, maxSpeed);
-        newVelocity = Mathf.Abs(newVelocity) < 0.05 ? 0 : newVelocity;
+        float newVelocity = Math.Clamp(rb.velocity.x + (HInput * acceleration) 
+                                        - ((onGround ? 1 : 0) * directionMultiplier * Mathf.Sqrt(groundFriction))
+                                        , -1 * maxSpeed, maxSpeed);
+        
+        newVelocity = Mathf.Abs(newVelocity) < 0.0001 ? 0 : newVelocity;
 
         rb.velocity = new Vector2(newVelocity, rb.velocity.y);
         
 
-        if(Input.GetButtonDown("Jump") && onGround) {
-            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
-        }
+
     }
 
+    private void setAnimations() {
+        
+    }
 
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Ground")) {
