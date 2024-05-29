5b7b520fc5bb2f09c8fb830419b559dbb0073640
TheShinySnivy <magicwand452@gmail.com>
2024-05-28 23:00:42 -0700

    text trigger changes, background changes

--- Assets/Scripts/UI/TutorialTextTrigger.cs
+++ Assets/Scripts/UI/TutorialTextTrigger.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Text.RegularExpressions;
 
 public class TutorialTextTrigger : MonoBehaviour
 {
@@ -12,18 +12,46 @@ public class TutorialTextTrigger : MonoBehaviour
     private string serializedTextField;
     private TextMeshProUGUI textDisplay;
 
+    private PlayerInput playerInput;
+    
+    private string _Movement_negative;
+    private string _Movement_positive;
+    private string _Jump;
+    private string _Crouch;
+    private string _ThrowRock;
+    private string _ToggleThrowRock;
+    private string _Pause;
+    private string _Toggle_Movement_negative;
+    private string _Toggle_Movement_positive;
+
 
     void Start()
     {
         textDisplay = TextDisplaySingleton.instance.GetComponent<TextMeshProUGUI>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && (canTriggerAgain || !alreadyTriggered))
         {
-            textDisplay.text = serializedTextField;
+            
+            textDisplay.text = ProcessString(serializedTextField);
             alreadyTriggered = true;
         }
+    }
+
+    private string ProcessString(string str) {
+        string pattern = @"&\w+"; // Matches an '&' followed by one or more word characters
+
+        MatchCollection matches = Regex.Matches(str, pattern);
+
+        foreach (Match match in matches)
+        {
+            string x = match.Value[1..].Split('_')[0];
+            str = str.Replace(match.Value, playerInput.actions["ToggleThrowRock"].bindings[0].path);
+            Debug.Log(match.Value + " modified " + x);
+        }
+        return str;
     }
 }
