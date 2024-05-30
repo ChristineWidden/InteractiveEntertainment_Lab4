d513d015ba8211a5908c573d80852e7cbee63bc1
TheShinySnivy <magicwand452@gmail.com>
2024-05-29 23:10:53 -0700

    updated todo

--- Assets/Scripts/MiscTODO.cs
+++ Assets/Scripts/MiscTODO.cs
@@ -2,7 +2,6 @@
 
 // https://www.youtube.com/watch?v=Hn804Wgr3KE
 // TODO: Make inputs for navigating menu match those for moving character
-// TODO: ADD A PAUSE MENU!!!
 // TODO: Cutscene between scenes? Maybe a fade transition?
 
 // TODO: Improve quality of transition to next level
